6ebd53b6fd37eae140f12d6827b9c67e76ea51eb
Tuan Luong <tuanluong.it@gmail.com>
2017-12-25 13:51:12 +0700

    Update logic to detect online/offline

--- app/src/main/java/com/ping/android/managers/UserManager.java
+++ app/src/main/java/com/ping/android/managers/UserManager.java
@@ -87,7 +87,7 @@ public class UserManager {
         userRepository.initializeUser((error, data) -> {
             if (error == null) {
                 user = (User) data[0];
-                userRepository.registerUserPresence();
+                //userRepository.registerUserPresence();
                 if (user.quickBloxID <= 0) {
                     quickBloxRepository.signUpNewUserQB(user, qbCallback);
                 } else {
@@ -97,11 +97,11 @@ public class UserManager {
                 callback.complete(error, data);
             }
         });
-        presenceRepository.listenStatusChange((error, data) -> {
-            if (error == null) {
-                userRepository.registerUserPresence();
-            }
-        });
+//        presenceRepository.listenStatusChange((error, data) -> {
+//            if (error == null) {
+//                userRepository.registerUserPresence();
+//            }
+//        });
     }
 
     private void onBlocksUpdated(Map<String, Object> blocks) {
@@ -199,6 +199,7 @@ public class UserManager {
     }
 
     public void logout(Context context) {
+        userRepository.deleteRefreshToken();
         FirebaseAuth auth = FirebaseAuth.getInstance();
         auth.signOut();
         CallService.logout(context);
