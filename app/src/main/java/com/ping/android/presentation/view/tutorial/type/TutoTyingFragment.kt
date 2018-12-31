ca485a5c49aeee5c69cbd5c6da1b3da6dd172ddf
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-31 15:55:22 +0700

    [DEV] show keyboard when click editext and toggle button mark message

--- app/src/main/java/com/ping/android/presentation/view/tutorial/type/TutoTyingFragment.kt
+++ app/src/main/java/com/ping/android/presentation/view/tutorial/type/TutoTyingFragment.kt
@@ -1,12 +1,18 @@
 package com.ping.android.presentation.view.tutorial.type;
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.ping.android.R
 import com.ping.android.presentation.view.fragment.BaseFragment
+import com.ping.android.utils.KeyboardHelpers
+import com.ping.android.utils.Log
 import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.view_chat_bottom.*
+import kotlinx.android.synthetic.main.view_tutorial_input_chat_bottom_typing.*
 
 /**
  * Created by Huu Hoang on 27/12/2018
@@ -14,7 +20,7 @@ import dagger.android.support.AndroidSupportInjection
 class TutoTyingFragment : BaseFragment(), TutoTypingContract.View {
 
     //region variable region
-
+    var isSelected = false;
     // endregion
 
     //region Android life cycle region
@@ -23,9 +29,17 @@ class TutoTyingFragment : BaseFragment(), TutoTypingContract.View {
         super.onCreate(savedInstanceState)
         AndroidSupportInjection.inject(this)
     }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_tuto_typing, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setUpview()
+    }
+
 
     // endregion
 
@@ -38,6 +52,32 @@ class TutoTyingFragment : BaseFragment(), TutoTypingContract.View {
     override fun hideLoading() {
         super<BaseFragment>.hideLoading()
     }
+    // endregion
+
+    //region private method region
+    private fun setUpview() {
+        tgMarkStatus.isSelected = false
+        updateMaskTintColor(tgMarkStatus.isSelected)
+        edt_emoji_msg.setOnClickListener {
+            KeyboardHelpers.showKeyboard(context, edt_emoji_msg)
+        }
+        // when click btn mark
+        tgMarkStatus.setOnClickListener {
+            tgMarkStatus.isSelected = !tgMarkStatus.isSelected
+            updateMaskTintColor(tgMarkStatus.isSelected)
+        }
+    }
+
+    private fun updateMaskTintColor(isEnable: Boolean) = if (!isEnable) {
+        val color = ContextCompat.getColor(context!!, R.color.color_grey)
+        tgMarkStatus.backgroundTintList = ColorStateList.valueOf(color)
+        isSelected = true;
+    } else {
+        val color = ContextCompat.getColor(context!!, R.color.black)
+        tgMarkStatus.backgroundTintList = ColorStateList.valueOf(color)
+        isSelected = false;
+    }
+
     // endregion
 
 }
