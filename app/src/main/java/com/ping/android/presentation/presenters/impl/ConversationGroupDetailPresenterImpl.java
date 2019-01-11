d0fa48e9bc9221dacf81f6ca54731373a66c6143
Huu Hoang <hoangduchuuvn@gmail.com>
2019-01-11 10:13:47 +0700

    [FIX] send message when user added

--- app/src/main/java/com/ping/android/presentation/presenters/impl/ConversationGroupDetailPresenterImpl.java
+++ app/src/main/java/com/ping/android/presentation/presenters/impl/ConversationGroupDetailPresenterImpl.java
@@ -152,11 +152,10 @@ public class ConversationGroupDetailPresenterImpl implements ConversationGroupDe
                 StringBuilder builder = new StringBuilder();
                 for (User u : selectedUsers) {
                     builder.append(userMapper.getUserDisPlay(u,conversation)).append(", ");
+                    String notificationBody = String.format("%s : %s  has joined ", conversationName, userMapper.getUserDisPlay(u,conversation));
+                    sendNotification(conversation, conversation.key, notificationBody);
+                    sendJoinedMessage(userMapper.getUserDisPlay(u,conversation));
                 }
-                String joinedUsers  = addedUsersStringBuilder.toString().substring(0,addedUsersStringBuilder.length()-2);
-                String notificationBody = String.format("%s : %s  has joined ", conversationName, joinedUsers);
-                sendNotification(conversation, conversation.key, notificationBody);
-                sendJoinedMessage(joinedUsers);
             // TODO compare to get exactly new User added
             }
 
